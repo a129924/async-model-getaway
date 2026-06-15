228cc19cff0a1a76a325243dc50dd88003a89af3
andrew <a129924@gmail.com>
2026-06-15 15:25:50 +0800

    chore: initialize project foundation

---
+++ src/async_model_gateway/__init__.py
@@ -0,0 +1,6 @@
+"""Foundation package for the async-model-gateway project."""
+
+
+def main() -> None:
+    """Run the minimal bootstrap entrypoint for the project scaffold."""
+    print("async-model-gateway foundation scaffold")
